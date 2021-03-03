0108831e8b53c26bfccac6b62338e9029b66323d
Arthur-art <arthurteixeira.guts@gmail.com>
2021-03-03 00:13:00 -0300

    Challenges and Exercises - Palindrome

--- Challenge-Progamathor/Palindrome.js
+++ Challenge-Progamathor/Palindrome.js
@@ -1,10 +1,6 @@
 /* escreva um script que crie uma matriz com 10.000 palavras aleatórias entre 3 e 5 
 caracteres e retorne o número de palavras que são palindromes   */
 
-function checkPalindrome(str) {
-    for(var i = 0; i < str.length / 2; i++) if (str[i] != str[str.length - i - 1]) return false;
-    return true;
-}
 
 const word = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
 
