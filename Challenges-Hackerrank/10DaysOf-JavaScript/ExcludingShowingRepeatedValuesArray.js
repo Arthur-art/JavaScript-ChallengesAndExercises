1aa4c2ac02ae810bdf12d42ffa9cf800cf8ea842
Arthur-art <arthurteixeira.guts@gmail.com>
2021-03-30 15:05:45 -0300

    refatorando ExcludingShowingRepeatedValuesArray

--- Challenges-Hackerrank/10DaysOf-JavaScript/ExcludingShowingRepeatedValuesArray.js
+++ Challenges-Hackerrank/10DaysOf-JavaScript/ExcludingShowingRepeatedValuesArray.js
@@ -20,23 +20,14 @@ const Utils={
     return valorRepetido;
   }
 }
-console.log(Utils.deletandoRepetidos(array2));
+
 
 //Retirando itens duplicados do array com filter
 const filterFunction = (()=>{
   let array = ["arthur", "ivar", "bk201", "arthur", "ivar"];
-  /*é checado se o elemento na primeira posicao que ele 
-  foi encontrado é === a posicao em que esta o indice position, se for filter recebe true e 
-  o element entra na variavel elementTrue, se não recebe false e é excluido de elementTrue*/
+  /*é checado se o indexOf do elemento é igual a posicao que ele foi encontrado*/
   let uniqueProducts = array.filter((element,position,array)=>{
-      let elementTrue =  array.indexOf(element) === position;
-      //console.log(elementTrue)
-      //console.log(element);
-      //console.log(position);
-      //console.log(array.indexOf(element));
-      return elementTrue;
+      return array.indexOf(element) === position;
   });
-
-  
-  console.log(uniqueProducts);
+  console.log(uniqueProducts)
 })();
